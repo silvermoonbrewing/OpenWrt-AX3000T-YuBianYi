d3bd9fad4164d41261ea1042808a3dec0e0b1ddf
silvermoonbrewing <149878727+silvermoonbrewing@users.noreply.github.com>
2024-11-13 13:20:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,3 +7,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.69.1/g' package/base-files/files/bin/config_generate
+sed -i 's/hostname='OpenWrt'/hostname='OpenWrt-XIAOMI-AX3000T'/g' package/base-files/files/bin/config_generate
